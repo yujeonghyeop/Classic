e7539c13f41c4075c6d030f296d2274f35dd0a47
yeiin <dldpdls17@ajou.ac.kr>
2022-05-08 18:07:17 +0900

    main flex

--- RNFB/RNFB/src/screens/HomeScreen.js
+++ RNFB/RNFB/src/screens/HomeScreen.js
@@ -5,61 +5,46 @@ import {View, Text, StyleSheet,Image, ScrollView} from 'react-native';
 import {Icon, Button} from 'react-native-elements';
 import HomeHeader from '../components/HomeHeader';
 import Profile from '../components/Profile';
-import logo from '../images/logo.png';
+import {logo} from '../global/styles';
+import Logo from '../images/logo.png';
 import {colors, parameters, title} from '../global/styles';
 import firestore, { firebase } from '@react-native-firebase/firestore'
 
 export default function HomeScreen({navigation}) {
   const [nickname, setNickname] = useState(null)
-    const [exp, setExp] = useState(null)
-    const user = firebase.auth().currentUser;
-    const email = user.email; 
-    const first = firestore().collection(email).doc("결과");
-    first.get().then((doc)=>{
-        if (doc.exists){
-            const pandi = doc.data();
-            const name = pandi["이름"];
-            const explain = pandi["설명"];
-            setNickname(name);
-            setExp(explain);
-        }
-    })
+  const [exp, setExp] = useState(null)
+  const user = firebase.auth().currentUser;
+  const email = user.email; 
+  const first = firestore().collection(email).doc("결과");
+  first.get().then((doc)=>{
+      if (doc.exists){
+          const pandi = doc.data();
+          const name = pandi["이름"];
+          const explain = pandi["설명"];
+          setNickname(name);
+          setExp(explain);
+      }
+  })
   return (
     <View style={styles.container}>
       {/* <HomeHeader navigation={navigation} /> */}
-      <Image source={logo} style={styles.logo} />
-      {/* <Profile style={{flex:1}}/>
-      profile 전시 */}
-      <View style = {{flex:1}}> 
+      <Image source={Logo} style={logo} />
       <View style={styles.styledBox}>
             <View style={{flexDirection:'row'}}>
-                <View style={{flexDirection: 'column', padding: 15}}> 
+                <View style={{flexDirection: 'column', margin: 10}}> 
                     <View style={styles.styledPicture}/>
-                    <Text
-                    style={{
-                        fontFamily: 'FrankRuhlLibre-Light',
-                    }}>{email}</Text>
+                    <Text style={{fontFamily: 'FrankRuhlLibre-Light',}}>{email}</Text>
                 </View>
-                <View style={{flex: 1, padding: 20, flexDirection:'column'}}>
-                    <Button
-                    title = {nickname}
-                    buttonStyle = {styles.nicknamebutton}
-                    titleStyle = {styles.nicknametitle}
-                        onPress = {() => {
-                            navigation.navigate("MyAccountScreen")
-                        }}
-                    />
-                    <Text
-                    style={{
-                        marginLeft:10,
-                        fontFamily: 'GowunBatang-Bold',
-                    }}>{exp}</Text>
+                <View style={{flex: 1,flexDirection:'column',alignItems:'center', margin:10}}>
+                    <Button title = {nickname} buttonStyle = {styles.nicknamebutton} titleStyle = {styles.nicknametitle}
+                        onPress = {() => { navigation.navigate("MyAccountScreen")}}/>
+                    <Text style={{fontFamily: 'GowunBatang-Bold'}}>{exp}</Text>
                     <Button title='상세보기' buttonStyle={styles.styledButton} titleStyle={styles.buttonTitle}/>
                 </View>
             </View> 
-        </View>
-        </View>
-      <Button
+      </View>
+      <View style = {{flex:1, alignItems:'center', margin:5}}>
+         <Button
         title="검사하러 가기"
         buttonStyle={styles.styledButton1}
         titleStyle={parameters.buttonTitle}
@@ -69,7 +54,8 @@ export default function HomeScreen({navigation}) {
         }}
         //이 버튼을 누를 때 SignInScreen으로 가라는 navigation 등록
       />
-      <View>
+      </View>
+      <View style={{flex:5, padding:5, marginBottom:5}}>
         <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
           <View style={styles.viewStyle}></View>
           <View style={styles.viewStyle}></View>
@@ -87,14 +73,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor:'#FFFFFF'
-    },
-    logo:{
-      width: 250,
-      height: 42,
-      alignSelf: 'center',
-      margin: 10,
-      marginTop: 40
+    backgroundColor:'#FFFFFF',
+    alignContent: 'center',
+    justifyContent: 'center',
     },
     nicknamebutton : {
       backgroundColor : '#E8E8F2'
@@ -105,7 +86,7 @@ const styles = StyleSheet.create({
       fontFamily: 'GowunBatang-Bold'
     },
     styledButton1: { //검사하러가기 버튼 스타일
-      //button 스타일 지정
+      //button 스타일 지정 
       backgroundColor: '#6667ab',
       alignContent: 'center',
       justifyContent: 'center',
@@ -113,10 +94,8 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       borderColor: '#6667ab',
       height: 50,
-      paddingHorizontal: 20,
-      width: '50%',
-      marginTop: 10,
-      marginLeft: 100,
+      width: 200,
+
     },
     viewStyle: { //Swipe 스타일
       backgroundColor : "#6667ab",
@@ -125,7 +104,8 @@ const styles = StyleSheet.create({
       height : 250,
       justifyContent: 'center',
       alignItems: 'center',
-      margin:20
+      marginTop:10,
+      marginLeft:10
     },
     styledPicture:{ 
       //사진 넣는 곳
@@ -136,23 +116,20 @@ const styles = StyleSheet.create({
   },
   styledBox: {  //box style
   //box
-    flex:1,
+    flex:3,
     backgroundColor: '#E8E8F2',
     alignContent: 'center',
     justifyContent: 'center',
     borderRadius: 20,
-    paddingHorizontal: 20,
-    height: 150,
-    marginTop: 10,
-    marginRight: 10,
-    marginLeft: 10
+    alignItems: 'stretch',
+    padding:20,
+    margin:10
   },
   styledButton:{  //상세보기 버튼 디자인
       backgroundColor: '#FFFFFF',
       opacity:0.3,
       borderRadius: 10,
-      width:200
-    
+      width:180
   },
   buttonTitle:{
       color:'black',
