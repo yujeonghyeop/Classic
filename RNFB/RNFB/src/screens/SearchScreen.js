bef69616c6671808bbfc9deaba51b73958ef96a0
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2022-05-14 22:31:45 +0900

    search logo

--- RNFB/RNFB/src/screens/SearchScreen.js
+++ RNFB/RNFB/src/screens/SearchScreen.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import { SearchBar } from 'react-native-elements';
+import {View, Text, StyleSheet, Image} from 'react-native'
+import {logo, myPageStyle} from '../global/styles';
+import { styledtext } from '../global/fontStyles';
+import Logo from '../images/logo.png';
 
 export default class App extends React.Component {
   state = {
@@ -14,11 +18,14 @@ export default class App extends React.Component {
     const { search } = this.state;
 
     return (
-      <SearchBar
+    <View style = {{flex:1, alignItems:'space-between', justifyContent:'center', flexDirection: 'column'}}>
+        <Image source={Logo} style={logo} />
+        <SearchBar
         placeholder="Type here..."
         onChangeText={this.updateSearch}
         value={search}
       />
+      </View>
     );
   }
 }
