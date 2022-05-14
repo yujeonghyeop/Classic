414fbb5e24dce993a54d1b4485ded1a0c7cbab9c
yeiin <dldpdls17@ajou.ac.kr>
2022-05-15 03:55:53 +0900

    temp1

--- RNFB/RNFB/src/screens/Setting.js
+++ RNFB/RNFB/src/screens/Setting.js
@@ -5,13 +5,14 @@ import {logo} from '../global/styles';
 import Logo from '../images/logo.png';
 import { styledtext } from '../global/fontStyles';
 
-export default function BusinessConsoleScreen(){
+export default function BusinessConsoleScreen({navigation}){
     return(
         <View style={{flexDirection:'column', justifyContent: 'space-evenly'}}>
             <Image source={Logo} style={logo} />
-            <View style = {{flexDirection:'column', backgroundColor : '#E8E8F2'}}>
-                <Button title="계정" buttonStyle ={style.button} titleStyle={styledtext}/>
-                <Button title="문의 또는 피드백하기" buttonStyle={style.button} titleStyle={styledtext}/>
+            <View style = {{flexDirection:'column', backgroundColor : '#E8E8F2',justifyContent: 'space-between' }}>
+                <Button title="계정" buttonStyle ={style.button} titleStyle={styledtext} onPress = {() => { navigation.navigate("Account")}}/>
+                <Button title="문의하기" buttonStyle ={style.button} titleStyle={styledtext} onPress = {() => { navigation.navigate("Inquiry")}}/>
+                <Button title="피드백하기" buttonStyle={style.button} titleStyle={styledtext} onPress = {() => { navigation.navigate("Feedback")}}/>
             </View>
         </View>
         
@@ -21,5 +22,9 @@ export default function BusinessConsoleScreen(){
 const style = StyleSheet.create({
     button:{
         backgroundColor: '#E8E8F2',
+        height:50,
+        paddingHorizontal:20,
+        width:"70%",
+        alignSelf: 'flex-start'
     }
 })
