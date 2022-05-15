f110c616c49581712a1d216334ee91ca4d0701c2
yeiin <dldpdls17@ajou.ac.kr>
2022-05-15 16:07:43 +0900

    temp

---
+++ RNFB/RNFB/src/screens/settingScreen/Logout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import{View, Text, StyleSheet, Image} from 'react-native'
+import {logo, myPageStyle} from '../../global/styles';
+import Logo from '../../images/logo.png'
+import {Button} from 'react-native-elements';
+import { buttonTitleB  } from '../../global/fontStyles';
+import {setting} from '../../global/styles';
+
+export default function Logout(){
+    return(
+        <View style = {setting.container}>
+            <Text>Logout</Text>
+        </View>
+    )
+}
