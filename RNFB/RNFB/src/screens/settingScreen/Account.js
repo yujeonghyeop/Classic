23c23f6111a87b081e4e6fdf8330e48c2b46f278
yeiin <dldpdls17@ajou.ac.kr>
2022-05-15 15:38:40 +0900

    setting base

---
+++ RNFB/RNFB/src/screens/settingScreen/Account.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import{View, Text, StyleSheet, Image} from 'react-native'
+import {logo, myPageStyle} from '../../global/styles';
+import Logo from '../../images/logo.png'
+import {Button} from 'react-native-elements';
+import { buttonTitleB  } from '../../global/fontStyles';
+import {setting} from '../../global/styles';
+
+export default function BusinessConsoleScreen({navigation}){
+    return(
+        <View style = {setting.container}>
+            <Image source={Logo} style={logo} />
+            <View style ={setting.AccountView}>
+                <Button title="passward 변경" buttonStyle={setting.AccountButton} titleStyle={buttonTitleB} />
+                <Button title="로그아웃" buttonStyle={setting.AccountButton} titleStyle={buttonTitleB } />
+            </View>
+        </View>
+    )
+}
