26eb84bddbd804b30a0235bb411483c5bc9332cf
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2022-05-19 20:57:17 +0900

    dd

--- RNFB/RNFB/src/screens/SearchScreen.js
+++ RNFB/RNFB/src/screens/SearchScreen.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { SearchBar } from 'react-native-elements';
 import {View, Text, Image} from 'react-native'
 import {logo} from '../global/styles';
-import { styledtext } from '../global/fontStyles';
 import Logo from '../images/logo.png';
 
 export default class App extends React.Component {
@@ -21,7 +20,7 @@ export default class App extends React.Component {
     <View>
         <Image source={Logo} style={logo} />
         <SearchBar
-        style={{fontFamily: 'IBMPlexSansKR-Regular', color: "#FF9D9D"}}
+        style={{/*fontFamily: 'IBMPlexSansKR-Regular', */color: "#FF9D9D"}}
         placeholder="Type here..."
         placeholderTextColor={'#FF9D9D'}
         inputContainerStyle={{backgroundColor: '#F2F2F2'}}
