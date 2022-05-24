50bfc98d7fd449444c734b253536a33ac4ced1d7
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2022-05-24 23:49:29 +0900

    Detail

--- RNFB/RNFB/src/screens/Detail.js
+++ RNFB/RNFB/src/screens/Detail.js
@@ -1,21 +1,27 @@
 // 상세보기 페이지
+
 // import React from 'react';
 import React,{useContext} from 'react';
-import{View,  StyleSheet, Image} from 'react-native'
-import {Button} from 'react-native-elements';
+import{View, Text, StyleSheet, Image} from 'react-native'
+//import {Button} from 'react-native-elements';
 import {logo} from '../global/styles';
 import Logo from '../images/logo.png';
-import { buttonTitleB } from '../global/fontStyles';
-import {setting} from '../global/styles';
-import auth from '@react-native-firebase/auth';
-import { SignInContext } from '../contexts/authContext';
+//import { buttonTitleB } from '../global/fontStyles';
+import {setting, myAccountstyle,} from '../global/styles';
+//import auth from '@react-native-firebase/auth';
+//import { SignInContext } from '../contexts/authContext';
 
 export default function Detail({navigation}){
 
 
     return(
-        <View >
-            
+        <View style = {myAccountstyle.container}>
+            <Image source={Logo} style={logo}/>
+            <View style={myAccountstyle.styledBox}>
+            <Text style={{color: '#6767A6', fontSize: 25, fontFamily: 'IBMPlexSansKR-Regular'}}>
+                여기에 넣어
+                </Text>
+            </View>
         </View>
         
     )
